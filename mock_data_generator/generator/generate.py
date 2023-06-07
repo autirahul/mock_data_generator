5540c3a3f6a5a257ddf46e1124b02ffa4ec5ee86
Rahul Auti <rahul.auti@thoughtworks.com>
2023-06-07 10:13:14 +0530

    Code refactor

--- mock_data_generator/generator/generate.py
+++ mock_data_generator/generator/generate.py
@@ -3,29 +3,7 @@ from faker import Faker
 from pandas import DataFrame
 from tqdm import tqdm
 
-
-def generate_string_data(fkr: Faker):
-    return fkr.pystr()
-
-
-def generate_integer_data(fkr: Faker):
-    return fkr.random_int()
-
-
-def generate_float_data(fkr: Faker):
-    return fkr.pyfloat()
-
-
-def generate_date_data(fkr: Faker):
-    return fkr.date()
-
-
-def generate_boolean_data(fkr: Faker):
-    return fkr.pybool()
-
-
-def generate_timestamp_data(fkr: Faker):
-    return fkr.date_time()
+from mock_data_generator.generator.predefined import _
 
 
 def save_output_as_csv(pd: DataFrame, output_path: str):
---
+++ mock_data_generator/generator/predefined.py
@@ -0,0 +1,25 @@
+def generate_string_data(fkr: Faker):
+    return fkr.pystr()
+
+
+def generate_integer_data(fkr: Faker):
+    return fkr.random_int()
+
+
+from faker import Faker
+
+
+def generate_float_data(fkr: Faker):
+    return fkr.pyfloat()
+
+
+def generate_date_data(fkr: Faker):
+    return fkr.date()
+
+
+def generate_boolean_data(fkr: Faker):
+    return fkr.pybool()
+
+
+def generate_timestamp_data(fkr: Faker):
+    return fkr.date_time()
